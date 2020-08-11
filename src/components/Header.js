2d0909559595e1af433a9bda5a91b0c56d2ef797
Earl Jay Caoile <unfie555@gmail.com>
2020-08-10 20:21:27 -0700

    pushing up crummy scaffold

---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import '../styles/header.scss';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+/**
+ * Simple header content that shows up on every page. Contains title and nav bar
+ *
+ * @component
+ * @example
+ * return (
+ *   <Header />
+ * )
+ */
+function Header() {
+  const links = [
+    { displayName: 'Home', url: '/' },
+    { displayName: 'Subpage', url: '/subpage' },
+  ];
+
+  const navLinks = [];
+
+  const [expanded, setExpanded] = useState(false);
+
+  for (let i = 0; i < links.length; i++) {
+    navLinks.push(
+      <Link
+        className="nav-link"
+        key={i}
+        to={links[i].url}
+        onClick={() => setTimeout(() => setExpanded(false), 100)}
+      >
+        {links[i].displayName}
+      </Link>
+    );
+  }
+
+  return (
+    <>
+      <Navbar id="main-header" variant="dark" expand="md" expanded={expanded}>
+        <Navbar.Brand>
+          <h1>Some App Name</h1>
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={() => setExpanded(!expanded)}
+        />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <>{navLinks}</>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
