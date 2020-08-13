899d8f2d0506966ef843dfb9e3bd19721e71953c
Earl Jay Caoile <unfie555@gmail.com>
2020-08-13 15:48:23 -0700

    using grid-like logic for recipes from search

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import { searchRecipes } from '../store/recipe-slice';
-import { Card, CardDeck, Button } from 'react-bootstrap';
+import { Card, CardDeck, Button, Col } from 'react-bootstrap';
 
 function RecipeList(props) {
   const { searchResults } = props;
@@ -11,17 +10,19 @@ function RecipeList(props) {
     const { recipe } = hit;
 
     recipesToRender.push(
-      <Card key={i}>
-        <Card.Img variant="top" src={recipe.image} />
-        <Card.Body>
-          <Card.Title>{recipe.label}</Card.Title>
-          <Card.Text>Source: {recipe.source}</Card.Text>
-          <Card.Text>View Details</Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <Button>Save to Favorites</Button>
-        </Card.Footer>
-      </Card>
+      <Col item xs={12} md={6} xl={3} key={i}>
+        <Card>
+          <Card.Img variant="top" src={recipe.image} />
+          <Card.Body>
+            <Card.Title>{recipe.label}</Card.Title>
+            <Card.Text>Source: {recipe.source}</Card.Text>
+            <Card.Text>View Details</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Button>Save to Favorites</Button>
+          </Card.Footer>
+        </Card>
+      </Col>
     );
   });
 
