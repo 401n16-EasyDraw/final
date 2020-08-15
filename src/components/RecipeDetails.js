890fc416f0af9c9ff627bcc42bc73e76351739b1
iptmh <tammyip@gmail.com>
2020-08-15 14:21:40 -0700

    update detail page

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { Card, CardDeck, Button, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function RecipeDetails(props) {
   const { activeRecipe } = props;
@@ -14,13 +16,31 @@ function RecipeDetails(props) {
 
   return Object.keys(activeRecipe).length ? (
     <>
-      <h2>{activeRecipe.label}</h2>
-      <p>Source: {activeRecipe.source}</p>
-      <div>{ingredientsToRender}</div>
+      <Link to="/" className="no-style">
+        <Button className="m-2" variant="info">
+          Back to Homepage
+                </Button>
+      </Link>
+      <Card>
+        <Card.Img variant="top" src={activeRecipe.image} />
+        <Card.Body>
+          <Card.Title>{activeRecipe.label}</Card.Title>
+          <Card.Text>Source: {activeRecipe.source}</Card.Text>
+          <Card.Text>
+            {ingredientsToRender}
+          </Card.Text>
+          <Card.Text>
+            <a href={activeRecipe.url}> Cooking Direction: {activeRecipe.url}</a>
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <Button className="btn-block">Save to Favorites</Button>
+        </Card.Footer>
+      </Card>
     </>
   ) : (
-    <Redirect push to="/" />
-  );
+      <Redirect push to="/" />
+    );
 }
 
 const mapStateToProps = (state) => {
