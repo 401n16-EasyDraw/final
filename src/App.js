7b2a55bb152baee9db6bbd0b0037ff717e2ae21e
smajumdar22 <smajumdar22@gmail.com>
2020-08-15 22:01:21 -0700

    Linkedin Auth in progress

--- src/App.js
+++ src/App.js
@@ -10,11 +10,27 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/base.scss';
 
+class FoodApp extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={apiResponse:""};
+
+    }
+    callAPI(){
+      fetch("https://lab12-oauth-linkedin.herokuapp.com/oauth")
+      .then(res=>res.text())
+      .then(res=>this.setstate({apiResponse:res}));
+    }
+    componentWillMount(){
+      this.callAPI();
+    }
+  }
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Header />
+        <div>{this.state.apiResponse}</div>
         <div id="main-content">
           <Route path="/" exact component={RecipeList} />
           <Route path="/recipes/details" exact component={RecipeDetails} />
