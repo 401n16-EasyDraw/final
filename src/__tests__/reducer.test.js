c2b9dda87b080de25a9a2580f5cd55fef13f914b
Earl Jay Caoile <unfie555@gmail.com>
2020-08-20 12:48:26 -0700

    actually added tests

--- src/__tests__/reducer.test.js
+++ src/__tests__/reducer.test.js
@@ -1,10 +1,11 @@
 import recipeReducer, {
   getRecipes,
   setActiveRecipe,
+  resetRecipeState,
 } from '../store/recipe-slice.js';
 const sampleData = require('./sample-data.json');
 
-describe('reducer', () => {
+describe('recipe reducer', () => {
   const recipes = [];
 
   sampleData.hits.forEach((hit) => {
@@ -34,5 +35,18 @@ describe('reducer', () => {
 
     newState = recipeReducer(newState, setActiveRecipe(recipes[2]));
     expect(newState.activeRecipe).toBe(recipes[2]);
+  });
+
+  it('can reset recipe state', () => {
+    let newState = recipeReducer(
+      {},
+      getRecipes({ searchResults: sampleData.hits, query: 'chicken' })
+    );
+    newState = recipeReducer(newState, setActiveRecipe(recipes[0]));
+    newState = recipeReducer(newState, resetRecipeState());
+
+    expect(newState.searchResults).toMatchObject([]);
+    expect(newState.query).toBe('');
+    expect(newState.activeRecipe).toMatchObject({});
   });
 });
